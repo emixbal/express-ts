047346c10e64d69cac123aace14a4d265ff73591
iqbal <emixbal@gmail.com>
2023-11-06 22:37:40 +0700

    user model

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
+const db = require("../db/models")
 
 class Controller {
     login(req: Request, res: Response): Response {
         return res.status(200).json({ "message": "ok", "data": req.body })
     }
     register(req: Request, res: Response): Response {
+        const { username, email, password } = req.body
+
+        const user = db.user.create({ username, email, password })
+
         return res.status(200).json({ "message": "ok", "data": req.body })
     }
 }
